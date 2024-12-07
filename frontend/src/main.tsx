c6d567f506d6f63150e346e1b687235284b5ae5b
Paloma Morais <paloma.morais5@gmail.com>
2024-12-07 17:00:40 -0300

    Add Pages folder and RegisterONG page
    - Created 'Pages' folder for HTML pages.
    - Added initial 'RegisterONG' page.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import RegisterONG from './pages/RegisterONG'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RegisterONG />
   </StrictMode>,
 )
---
+++ frontend/src/pages/RegisterONG/index.tsx
@@ -0,0 +1,10 @@
+function RegisterONG(){
+
+    return (
+        <div>
+            <h1>RegisterONG</h1>
+        </div>
+    )
+}
+
+export default RegisterONG 
