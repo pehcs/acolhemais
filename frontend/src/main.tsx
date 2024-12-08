8798402549d5fdcdcc71f64d200557f5996bff9e
Paloma Morais <paloma.morais5@gmail.com>
2024-12-08 11:59:03 -0300

    Add react-router-dom dependency, button in CadastroONG, create NomeONG.tsx, and Dockerfile comments
    - Added "react-router-dom": "^6.16.0" dependency to package.json.
    - Added button in CadastroONG component.
    - Created file NomeONG.tsx.
    - Added comments to Dockerfile for better documentation.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import CadastroONG from './pages/CadastroONG'
-import '../index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CadastroONG from './pages/CadastroONG';
+import NomeONG from './pages/NomeONG';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CadastroONG />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CadastroONG />} />
+        <Route path="/NomeONG" element={<NomeONG />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
