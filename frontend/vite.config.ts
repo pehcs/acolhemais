df188c1abb6cab9c57b24663749a777e184968ea
Pedro <pehcorreias@gmail.com>
2025-02-03 09:14:43 -0300

    chore(react): vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,24 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss()]
-    }
-  },
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    css: {
+        postcss: {
+            plugins: [tailwindcss()]
+        }
     },
-  },
+    plugins: [react()],
+    server: {
+        host: true,
+        strictPort: true,
+        allowedHosts: ['acolhemais.vombate.com.br'],
+        port: 3000
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
