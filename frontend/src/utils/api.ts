1d0ba8a906a9cb332a4dc25a8d81bcf9da7f921a
Pedro <pehcorreias@gmail.com>
2025-02-03 09:12:57 -0300

    fix(auth): estava usando o cookie errado para autenticaçao

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -10,9 +10,9 @@ const api = axios.create({
 api.interceptors.response.use(
     response => response,
     error => {
-        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-            window.location.href = "/login";
-        }
+        // if (error.response.status === 401 || error.response.status === 403) {
+        //     window.location.href = "/login";
+        // }
         return Promise.reject(error);
     }
 );
