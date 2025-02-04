aa97153f7296de63fa0f4e94f06216150d58088c
Pedro <pehcorreias@gmail.com>
2025-02-03 22:46:19 -0300

    fix(ong): removendo bugs

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -19,9 +19,9 @@ api.interceptors.request.use(
 api.interceptors.response.use(
     response => response,
     error => {
-        // if (error.response.status === 401 || error.response.status === 403) {
-        //     window.location.href = "/login";
-        // }
+        if (error.response.status === 401 || error.response.status === 403) {
+            window.location.href = "/login";
+        }
         return Promise.reject(error);
     }
 );
