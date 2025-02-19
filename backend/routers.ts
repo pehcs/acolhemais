689b724665cf4411ce174da00aa7b802960ec327
Pedro <pehcorreias@gmail.com>
2025-02-19 17:29:14 -0300

    fix(acao): não permitia usuário não logado ver as ações

--- backend/routers.ts
+++ backend/routers.ts
@@ -20,8 +20,8 @@ router.post('/v1/ong/:id/contact', authMiddleware, ONGController.addContact);
 router.delete('/v1/ong/contact/:id', authMiddleware, ONGController.removeContact);
 
 router.post('/v1/ong/:id/acoes', authMiddleware, ONGAcaoController.create);
-router.get('/v1/ong/:id/acoes', authMiddleware, ONGAcaoController.findAllByOng);
-router.get('/v1/acoes/:id', authMiddleware, ONGAcaoController.findById);
+router.get('/v1/ong/:id/acoes', ONGAcaoController.findAllByOng);
+router.get('/v1/acoes/:id', ONGAcaoController.findById);
 router.delete('/v1/acoes/:id', authMiddleware, ONGAcaoController.delete);
 router.put('/v1/acoes/:id', authMiddleware, ONGAcaoController.update);
 router.post('/v1/acoes/:id/banner', upload.single('banner'), ONGAcaoController.changeBanner);
